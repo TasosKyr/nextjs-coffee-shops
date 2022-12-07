44f1f55c02defaec40d4f14c983d8ea88127e8d2
Tasos Kyr <tasoskyr1@gmail.com>
2022-12-07 19:14:32 +0100

    add styling in banner

--- components/banner.js
+++ components/banner.js
@@ -1,13 +1,18 @@
 import styles from "../styles/Banner.module.css";
 
-const Banner = ({buttonText, handleOnClick}) => {
+const Banner = ({ buttonText, handleOnClick }) => {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>
-        <span className={styles.title1}>Coffee</span> <span className={styles.title2}>shops</span>
+        <span className={styles.title1}>Coffee</span>{" "}
+        <span className={styles.title2}>shops</span>
       </h1>
       <p className={styles.subtitle}>Discover your local coffee places!</p>
-      <button className={styles.button} onClick={handleOnClick}>{buttonText}</button>
+      <div className={styles.buttonWrapper}>
+        <button className={styles.button} onClick={handleOnClick}>
+          {buttonText}
+        </button>
+      </div>
     </div>
   );
 };
--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,9 +1,13 @@
-import { useRouter } from "next/router"
+import { useRouter } from "next/router";
+import Link from "next/link";
 
 export default function CoffeeStore() {
-  const router = useRouter()
-  console.log("🚀 ~ file: [id].js:5 ~ CoffeeStore ~ router", router)
+  const router = useRouter();
+  console.log("🚀 ~ file: [id].js:5 ~ CoffeeStore ~ router", router);
   return (
-    <div>Coffee Store {router.query.id}</div>
-  )
+    <div>
+      Coffee Store {router.query.id}
+      <Link href="/">BACK TO HOME</Link>
+    </div>
+  );
 }
